d4f013609311a5edd4abaa517c23532d86e382b1
RyuNen344 <s1100633@outlook.com>
2022-03-13 00:45:37 +0900

    iroiro update

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,12 @@
+declare const profile: {
+    name: string;
+    sex: string;
+    location: string;
+};
+declare const fooBar: (a: number, b: string, c: typeof profile) => void;
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+declare function greeter(person: Person): string;
 //# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,15 @@
 "use strict";
-console.log('Hello!');
 console.log("Hello!");
 console.log("Hello!");
+console.log("Hello!Hoge");
+const profile = { name: "soarflat", sex: "male", location: "Tokyo" };
+const fooBar = (a, b, c) => {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+};
+fooBar(111, "hoge!", profile);
+function greeter(person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+//# sourceMappingURL=index.js.map
