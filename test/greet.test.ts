04b2497d15134306d8695a0cba444bdd9ce7d7b4
RyuNen344 <s1100633@outlook.com>
2022-03-17 03:45:19 +0900

    remove unsused file

--- test/greet.test.ts
+++
@@ -1,8 +0,0 @@
-import greet from "@/greet"
-
-describe("should say fugafuga", () => {
-    test('should say hello to Tom.', () => {
-        const response = greet('Tom');
-        expect(response).toBe('Hello, Tom!');
-    })
-});
