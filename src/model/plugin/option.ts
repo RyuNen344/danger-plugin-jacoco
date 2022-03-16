33aff445cca14591c5f7e152c45ba488e4cb8448
RyuNen344 <s1100633@outlook.com>
2022-03-17 03:45:34 +0900

    remove file extension option

--- src/model/plugin/option.ts
+++ src/model/plugin/option.ts
@@ -2,12 +2,10 @@ export interface Option {
     projectCoverageRate: number;
     exportUnit: EXPORT_UNIT;
     excludePackages: string[];
-    filesExtensions: string[];
 }
 
 export const defaultOption: Option = {
     projectCoverageRate: 0,
     exportUnit: EXPORT_UNIT.FILE,
     excludePackages: [],
-    filesExtensions: ["java", "kt"],
 };
