5ea3af7b4f7add8a8957d024b13fddb946cf81b2
RyuNen344 <s1100633@outlook.com>
2022-03-17 02:31:25 +0900

    fix unwrap optional

--- test/collector/collector_method.test.ts
+++ test/collector/collector_method.test.ts
@@ -1,29 +1,29 @@
-import { MethodCollector } from '@/collector/collector_method';
-import { ProjectCollector } from '@/collector/collector_project';
-import { importXml } from '@/file/parser_xml';
-import { Method } from '@/model/jacoco/method';
-import { Type } from '@/model/jacoco/type';
-import * as fs from 'fs';
-import { Logger } from 'tslog';
+import { MethodCollector } from "@/collector/collector_method";
+import { ProjectCollector } from "@/collector/collector_project";
+import { importXml } from "@/file/parser_xml";
+import { Method } from "@/model/jacoco/method";
+import { Type } from "@/model/jacoco/type";
+import * as fs from "fs";
+import { Logger } from "tslog";
 
 const log = new Logger();
 
 describe("import xml file and deserialize it", () => {
-    test('should success modeling from xml', () => {
-        const xml = fs.readFileSync("test/__resource__/sample_jacoco_dagashi.xml")
+    test("should success modeling from xml", () => {
+        const xml = fs.readFileSync("test/__resource__/sample_jacoco_dagashi.xml");
         const jacoco = importXml(xml);
 
         const coverage = new ProjectCollector(jacoco.report).collect();
-        log.debug('name ' + coverage.name);
-        log.debug(`instructionsCov ${coverage.instructionsCov || "N/A"}`);
-        log.debug(`branchesCov ${coverage.branchesCov || "N/A"}`);
-        log.debug(`complexityRate ${coverage.complexityRate || "N/A"}`);
-        log.debug(`coveredLinesRate ${coverage.coveredLinesRate || "N/A"}`);
-        log.debug(`coveredMethodsRate ${coverage.coveredMethodsRate || "N/A"}`);
-        log.debug(`coveredClassesRate ${coverage.coveredClassesRate || "N/A"}`);
+        log.debug(coverage.name);
+        log.debug(`instructionsCov ${coverage.instructionsCov != null ? coverage.instructionsCov : "N/A"}`);
+        log.debug(`branchesCov ${coverage.branchesCov != null ? coverage.branchesCov : "N/A"}`);
+        log.debug(`complexityRate ${coverage.complexityRate != null ? coverage.complexityRate : "N/A"}`);
+        log.debug(`coveredLinesRate ${coverage.coveredLinesRate != null ? coverage.coveredLinesRate : "N/A"}`);
+        log.debug(`coveredMethodsRate ${coverage.coveredMethodsRate != null ? coverage.coveredMethodsRate : "N/A"}`);
+        log.debug(`coveredClassesRate ${coverage.coveredClassesRate != null ? coverage.coveredClassesRate : "N/A"}`);
     });
 
-    test('should success a.xml', () => {
+    test("should success a.xml", () => {
         const method: Method = {
             name: "invokeSuspend",
             desc: "(Ljava/lang/Object;)Ljava/lang/Object;",
@@ -32,34 +32,33 @@ describe("import xml file and deserialize it", () => {
                 {
                     covered: 6440,
                     missed: 16833,
-                    type: Type.Instruction
+                    type: Type.Instruction,
                 },
                 {
                     covered: 948,
                     missed: 2784,
-                    type: Type.Line
+                    type: Type.Line,
                 },
                 {
                     covered: 401,
                     missed: 1501,
-                    type: Type.Complexity
+                    type: Type.Complexity,
                 },
                 {
                     covered: 324,
                     missed: 909,
-                    type: Type.Method
-                }
-            ]
+                    type: Type.Method,
+                },
+                {
+                    covered: 94,
+                    missed: 320,
+                    type: Type.Branch,
+                },
+            ],
         };
 
         const collcetor = new MethodCollector(method);
         const coverage = collcetor.collect();
-        log.debug(coverage.name);
-        log.debug(coverage.instructionsCov);
         log.debug(coverage.branchesCov);
-        log.debug(coverage.complexityRate);
-        log.debug(coverage.coveredLinesRate);
-        log.debug(coverage.coveredMethodsRate);
-        log.debug(coverage.coveredClassesRate);
     });
 });
