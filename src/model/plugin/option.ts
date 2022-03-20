c2718695eea3b78f364d65f52a3c7100715a2fab
RyuNen344 <s1100633@outlook.com>
2022-03-21 03:39:56 +0900

    add export option

--- src/model/plugin/option.ts
+++ src/model/plugin/option.ts
@@ -4,10 +4,12 @@ export interface Option {
     projectCoverageRate: number;
     exportUnit: ExportUnit;
     excludePackages: string[];
+    exportAll: boolean;
 }
 
 export const defaultOption: Option = {
     projectCoverageRate: 0,
     exportUnit: EXPORT_UNIT.FILE,
     excludePackages: [],
+    exportAll: false,
 };
